804c1812cba974dfb0b856e0fef0eb761c51357c
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2023-05-26 18:49:28 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Heavyrain PHP - Loadtest tool
+
+Heavyrain is loadtest/stresstest tool made with PHP.
+
+## Install
+
+## Usage
+
+```sh
+$ docker run TODO
+```
---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
+namespace Heavyrain;
+
+use PHPUnit\Framework\TestCase as FrameworkTestCase;
+
+abstract class TestCase extends FrameworkTestCase
+{
+}
