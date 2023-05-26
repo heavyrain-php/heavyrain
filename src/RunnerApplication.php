a5f1520fea4c086e19276223c2d4ce6174161064
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2023-05-26 19:02:37 +0900

    feat(Console): Add Application

---
+++ src/RunnerApplication.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
+namespace Heavyrain;
+
+use Symfony\Component\Console\Application;
+
+final class RunnerApplication extends Application
+{
+}
